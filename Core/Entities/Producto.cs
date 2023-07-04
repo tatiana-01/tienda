de117b46640e4076ef72be7fd25742eda57ba6fb
Tatiana <tatianacalderon10@gmail.com>
2023-07-04 09:36:21 -0500

    feat: :tada: Se inicio proyecto con entity framework

---
+++ Core/Entities/Producto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string ? Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
---
+++ Infrastructure/Data/TiendaContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class TiendaContext : DbContext
+    {
+        public TiendaContext(DbContextOptions<TiendaContext> options) : base(options)
+        {
+        }
+        public DbSet<Producto> Productos { get; set; }
+        
+    }
+}
